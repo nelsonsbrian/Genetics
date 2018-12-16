5ac4b1df073eb54e77d9685d2e14a20786e97e65
Brian Nelson <nelsonsbrian@gmail.com>
2018-12-15 16:31:08 -0800

    add styles to display data

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -16,13 +16,13 @@ class Data extends React.Component {
         "ToBe": ToBe
       },
       iterations: [],
-      this.handleSubmit = this.handleSubmit.bind(this);
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleIterations = this.handleIterations.bind(this);
   }
   handleChange(event) {
-    this.setState({nextTest: event.target.value })
+    this.setState({ nextTest: event.target.value })
   }
   handleSubmit(event) {
     event.preventDefault();
@@ -31,28 +31,39 @@ class Data extends React.Component {
   }
   handleIterations(newIteration) {
     const newState = [...this.state.iterations, newIteration];
-    this.setState({ iterations: newState});
-    console.log(this.state.iterations);
+    this.setState({ iterations: newState });
   }
 
   render() {
-    const {nextTest, tests, currentTest, iterations} = this.state;
+    const { nextTest, tests, currentTest, iterations } = this.state;
     return (
-      <div>
-        <button onClick={this.testFunc}>click</button>
-        <form value={nextTest} onSubmit={this.handleSubmit}>
-          <label>
-            Pick your favorite flavor:
+      <div className='container'>
+        <style jsx>{`
+            .col {
+              display: inline-block;
+              background-color: lightblue;
+            }
+          `}</style>
+        <div className='row'>
+
+          <div className='col'>
+            <form value={nextTest} onSubmit={this.handleSubmit}>
+              <label>
+                Pick your favorite flavor:
           <select onChange={this.handleChange}>
-              <option value="RocketGA">genetic algorithm</option>
-              <option value="ToBe">ToBe</option>
-            </select>
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-        {this.state.currentTest ? <P5Wrapper sketch={tests[currentTest]} handleIterations={this.handleIterations}/> : null}
-        <DisplayData iterations={iterations}/>
-        <ChangeTest />
+                  <option value="RocketGA">genetic algorithm</option>
+                  <option value="ToBe">ToBe</option>
+                </select>
+              </label>
+              <input type="submit" value="Submit" />
+            </form>
+            {this.state.currentTest ? <P5Wrapper sketch={tests[currentTest]} handleIterations={this.handleIterations} /> : null}
+          </div>
+          <div className='col'>
+            <DisplayData iterations={iterations} />
+            <ChangeTest />
+          </div>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 import './p5/p5.dom.js';
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
